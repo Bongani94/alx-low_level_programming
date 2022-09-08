1a9e00fa73930d5c6702c3829f05c9329b729d5f
Bongani <bonganibaloyi94@gmail.com>
2022-09-08 14:39:30 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
 * main - print the string in the put function
-*
+* 
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
@@ -22,3 +22,4 @@ int main(void)
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
+^C
