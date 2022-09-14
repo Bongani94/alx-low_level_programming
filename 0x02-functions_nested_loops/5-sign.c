979eb9dbc23c43f63131e3699faa91e80199c403
Bongani <bonganibaloyi94@gmail.com>
2022-09-14 07:16:32 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar('+');
-	return (1);
-}
+		_putchar('+');
+		return (1);
+	}
 
 	else if (n < 0)
 	{
-	_putchar('_');
-	return (-1);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-	_putchar('0');
-	return (0);
+		_putchar('0');
+		return (0);
 	}
-
 }
