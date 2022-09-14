c486d0272b9e30e065ba6da5e00668d38ea4f59d
Bongani <bonganibaloyi94@gmail.com>
2022-09-14 06:19:08 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,20 +14,20 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59)
-		{
-			mins_remainder = minutes % 10;
-			hours_remainder = hours % 10;
-			hours_remainder = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0')
-				_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
-			minutes++;
-			_putchar('\n');
-		}
-		hours++;
-		minutes = 0;
+	while (minutes <= 59)
+	{
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0')
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0');
+	minutes++;
+	_putchar('\n');
 	}
+	hours++;
+	minutes = 0;
+}
 }
