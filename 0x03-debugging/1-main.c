2d701b32237de8a71750c5bdb3a7b09ed772f64f
Bongani <bonganibaloyi94@gmail.com>
2022-09-14 23:45:56 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 /*
-* while (i < 10)
-* {
-* putchar(i);
-* }
+*while (i < 10)
+*{
+*putchar(i);
+*}
 */
 printf("Infinite loop avoided! \\o/\n");
 
