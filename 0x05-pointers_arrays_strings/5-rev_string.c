fa65ea4568841c1de8585643b93b0dff3e527886
Bongani <bonganibaloyi94@gmail.com>
2022-09-19 18:07:08 -0700

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 		while (s[length1] != '\0')
 			length1++;
 
-		length2 = length1 - 1
+		length2 = length1 - 1;
 			for (i = 0; i < length1 / 2; i++)
 			{
-				"samalx" "xlamas"
+				"samalx" "xlamas";
 					tmp = s[i];
 				s[i] = s[length2];
 				s[length2] = tmp;
