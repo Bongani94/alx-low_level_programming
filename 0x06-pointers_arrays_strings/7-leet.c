8669169cd0796968ef3a4b65938e0478989083cf
Bongani <bonganibaloyi94@gmail.com>
2022-09-24 11:08:48 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,19 @@
 
 char *leet(char *str)
 {
-	int index1 = 0, index2;
+	int indx1 = 0, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', 'T'};
 
-	while (str[++index1])
+	while (str[++indx1])
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (str[index1] == leet[index2] ||
-					str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (str[indx1] == leet[indx2] ||
+					str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
 		}
 
-		index1++;
+		indx1++;
 	}
 
 	return (str);
