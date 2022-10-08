080f34967b9aca0691632681f1f947e0a1025d74
Bongani <bonganibaloyi94@gmail.com>
2022-10-08 03:57:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
