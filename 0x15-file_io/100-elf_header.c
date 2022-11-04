58273489ed4d0f0d80a24ee8ae177edf9ebeba29
Bongani <bonganibaloyi94@gmail.com>
2022-11-04 15:30:32 -0700

    elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,7 +23,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
-  *chec_elf - Checks if a file is an ELF file.
+  *check_elf - Checks if a file is an ELF file.
   *@e_ident: A pointer to an array containing the ELF magic numbers.
   *
   *Description: If the file is not an ELF file - exit code 98.
