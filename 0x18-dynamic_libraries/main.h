f5af862d9c30d9f84a165816756fad1cff2663ee
Bongani <bonganibaloyi94@gmail.com>
2022-12-13 09:36:40 -0800

    task 0

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef _SHARED_LIB_H_
-#define _SHARED_LIB_H_
-
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* _SHARED_LIB_H_ */
+#endif /* _MAIN_H_ */
